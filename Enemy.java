cf909abc2d4d03c28cc5aa4b38fc7df72b946b37
zbrewer42 <94584260+zbrewer42@users.noreply.github.com>
2021-12-13 11:23:40 -0600

    Added enemy player collisions

--- Enemy.java
+++ Enemy.java
@@ -1,35 +1,33 @@
-package Game;
+package NewGame;
 
-import java.awt.Color;
+import java.awt.*;
 import java.awt.Graphics;
 
 public class Enemy extends GameObject {
 	int type;
 	boolean alive;
-
 	public Enemy(int ex, int why, int wid, int hi) {
 		super(ex, why, wid, hi);
 		type = 1;
 		alive = true;
-
+		
 	}
-
 	public Enemy(int ex, int why, int wid, int hi, int t) {
 		super(ex, why, wid, hi);
 		type = t;
 		alive = true;
-
+		
 	}
-
 	public void render(Graphics g) {
-		if (alive) {
+		if(alive) {
 			g.setColor(Color.BLUE);
 			g.fillOval(x, y, w, h);
 			tick();
 		}
-		if (Game.p.s.hitE(this)) {
-			alive = false;
-		}
+		if(Game.p.s.hitE(this)) {alive = false;}
+		if(this.collide(Game.p)) {Game.p.health = 0;}
 	}
+	
+	
 
 }
