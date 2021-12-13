905b657cd8d00c22c9472f4641affd2f799d6db2
zbrewer42 <94584260+zbrewer42@users.noreply.github.com>
2021-12-13 11:26:33 -0600

    12/13/21
    Fixed Bullets pt 2

--- Bullet.java
+++ Bullet.java
@@ -1,32 +1,30 @@
-package Game;
-
-import java.awt.Color;
-import java.awt.Graphics;
-
-public class Bullet extends Weapon {
-
-	public Bullet(int ex, int why) {
+package NewGame;
+import java.awt.*;
+public class Bullet extends Weapon{
+	int dir = 0;
+	public Bullet(int ex, int why, int d) {
 		super(ex, why, 5, 5);
+		dir = d;
 		dx = 2;
-
+		
+		
 	}
-
 	public void render(Graphics g) {
-
+		
 		g.setColor(Color.WHITE);
 		g.fillOval(x, y, w, h);
 		tick();
 	}
-
 	public void tick() {
-		if (Game.p.g.direction == 1)
+		if(dir == 1)
 			y -= 2;
-		if (Game.p.g.direction == 2)
-			y += 2;
-		if (Game.p.g.direction == 3)
+		if(dir == 2)
+			y += 2;	
+		if(dir == 3)
 			x += 2;
-		if (Game.p.g.direction == 4)
+		if(dir == 4)
 			x -= 2;
 	}
-
-}
+	
+	
+	}
