9f06961a86cc78354c6cc9f259eef1a8a169bf28
zbrewer42 <94584260+zbrewer42@users.noreply.github.com>
2022-01-10 09:06:19 -0600

    1/10/22
    Updates to work with both the player and the enemy.

--- Bullet.java
+++ Bullet.java
@@ -1,7 +1,7 @@
 package NewGame;
 import java.awt.*;
 public class Bullet extends Weapon{
-	int dir = 0;
+	int dir = 0; //1 is up, 2 is down, 3 is right, 4 is left
 	public Bullet(int ex, int why, int d) {
 		super(ex, why, 5, 5);
 		dir = d;
@@ -9,6 +9,12 @@ public class Bullet extends Weapon{
 		
 		
 	}
+	public Bullet(int ex, int why) {
+		super(ex, why, 10, 10);
+		dx= 2;
+		dir = 0;
+	}
+	public void reset(int ex, int why) {x = ex; y = why;}
 	public void render(Graphics g) {
 		
 		g.setColor(Color.WHITE);
@@ -24,6 +30,24 @@ public class Bullet extends Weapon{
 			x += 2;
 		if(dir == 4)
 			x -= 2;
+		//else {x += dx; y+= dy;}	
+	}
+	
+	public boolean touches(Player p) {
+		//right border
+		if(x+w == p.x && y > p.y && y < p.y+p.h)
+			return true;
+		//left border
+		else if(x == p.x+w && y > p.y && y < p.y+p.h) 
+			return true;
+		//top border
+		else if(y == p.y+p.h && x > p.x && x < p.x+w)
+			return true;
+		//bottom border
+		else if(y+h == p.y && x > p.x && x < p.x+w)
+			return true;
+		return false;
+		
 	}
 	
 	
