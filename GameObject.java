053c5d54e6a52387578c8cf8d1ef8e1d6f67f1c9
Bobcatadam537 <89475624+Bobcatadam537@users.noreply.github.com>
2022-01-14 11:27:48 -0600

    1/14/22

--- GameObject.java
+++ GameObject.java
@@ -1,22 +1,30 @@
+package game;
+
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class GameObject {
 	Rectangle2D hitbox;
 	float velocity;
 	double angle = 0; // angles are in radians
+	LinkedList<Line2D> touching;
 
 	public GameObject(int x, int y, int w, int h) {
 		hitbox = new Rectangle2D.Float(x, y, w, h);
+		touching = new LinkedList<Line2D>();
 	}
 
-	public Line2D collide(Solid s) {
-		for (Line2D l : s.lines) {
-			if (l.intersects(hitbox)) {
-				return l;
+	public LinkedList<Line2D> collide(ArrayList<Solid> S) {
+		touching = new LinkedList<Line2D>();
+		for (Solid s : S) {
+			for (Line2D line : s.lines) {
+				if (line.intersects(hitbox))
+					touching.add(line);
 			}
 		}
-		return null;
+		return touching;
 	}
 
 	public void tick() {
@@ -25,5 +33,9 @@ public class GameObject {
 
 	public void scroll(float mx, float my) {
 		hitbox.setRect(hitbox.getMinX() + mx, hitbox.getMinY() + my, hitbox.getWidth(), hitbox.getHeight());
+	}
+
+	public void aimAt(GameObject o) {
+		angle = Math.atan2(o.hitbox.getCenterY() - hitbox.getCenterY(), o.hitbox.getCenterX() - hitbox.getCenterX());
 	}
 }
