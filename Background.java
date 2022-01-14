fb3c6d13ce89e473202c516f35f4dc4ac4cfc3c7
Bobcatadam537 <89475624+Bobcatadam537@users.noreply.github.com>
2022-01-14 11:39:36 -0600

    1/14/22 (Initial Commit)

---
+++ Background.java
@@ -0,0 +1,19 @@
+package game;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+public class Background extends GameObject {
+	Image image;
+
+	public Background(Image i) {
+		super(0, 0, 0, 0);
+		image = i;
+		hitbox.setRect(0, 0, image.getWidth(null), image.getHeight(null));
+	}
+
+	public void render(Graphics2D g) {
+		g.drawImage(image, (int) hitbox.getX(), (int) hitbox.getY(), image.getWidth(null), image.getHeight(null), null);
+	}
+
+}
