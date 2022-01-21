1a52d5ab8f0702c1955dd3b23100c4649f9e856d
Bobcatadam537 <89475624+Bobcatadam537@users.noreply.github.com>
2022-01-21 10:53:38 -0600

    1/19/22

--- Character.java
+++ Character.java
@@ -1,21 +1,63 @@
 package game;
 
+import java.awt.Image;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
 public class Character extends GameObject {
+	enum direction {
+		up(0), upright(4), right(8), downright(12), down(16), downleft(20), left(24), upleft(28);
+
+		public int dir;
+
+		direction(int i) {
+			dir = i;
+		}
+	}
+
+	enum action {
+		none(0), shoot(1), melee(2), dodge(3), hurt(4);
+
+		public int val;
+
+		action(int i) {
+			val = i;
+		}
+	}
+
+	enum weapon {
+		gun(1), sword(2);
+
+		public int val;
+
+		weapon(int i) {
+			val = i;
+		}
+	}
+
 	float vx, vy;
 	int W, H;
 	float speed = 0.5f;
 	int health;
 	int cooldown;
+	action a;
+	direction d;
+	weapon w;
+	Line2D melee;
 	Game game;
 
-	public Character(int x, int y, int w, int h, Game g) {
-		super(x, y, w, h);
-		W = w;
-		H = h;
+	Image[][] sprites;
+
+	public Character(int X, int Y, int W, int H, Game g) {
+		super(X, Y, W, H);
+		this.W = W;
+		this.H = H;
 		game = g;
+		a = action.none;
+		d = direction.up;
+		w = weapon.gun;
+		melee = null;
+		sprites = null;
 	}
 
 	public void trigMove() {
@@ -83,4 +125,34 @@ public class Character extends GameObject {
 			}
 		}
 	}
+
+	public void shoot(int c) {
+		if (cooldown == 0) {
+			a = action.shoot;
+			game.bullets.add(new Bullet(this, (int) hitbox.getCenterX(), (int) hitbox.getCenterY(), angle));
+			cooldown = c;
+		}
+	}
+
+	public void melee(int c) {
+		if (cooldown == 0) {
+			a = action.melee;
+			cooldown = c;
+		}
+	}
+
+	public void getDirection() {
+		String temp = "";
+		if (Math.toDegrees(angle) >= -90 - 45 && Math.toDegrees(angle) <= -90 + 45)
+			temp += "up";
+		if (Math.toDegrees(angle) >= 90 - 45 && Math.toDegrees(angle) <= 90 + 45)
+			temp += "down";
+		if (Math.toDegrees(angle) >= 0 - 45 && Math.toDegrees(angle) <= 0 + 45)
+			temp += "right";
+		if (Math.toDegrees(angle) >= 180 - 45 || Math.toDegrees(angle) <= -180 + 45)
+			temp += "left";
+
+		d = direction.valueOf(temp);
+	}
+
 }
