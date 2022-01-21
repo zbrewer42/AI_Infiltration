f501f43cae44ce1bf4a845cae48af462215ac2dd
Bobcatadam537 <89475624+Bobcatadam537@users.noreply.github.com>
2022-01-21 10:56:23 -0600

    1/19/22

--- Player.java
+++ Player.java
@@ -2,7 +2,11 @@ package game;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.geom.Line2D;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 class Player extends Character {
 	// does not utilize the velocity or angle how the A.I. classes do.
@@ -11,31 +15,38 @@ class Player extends Character {
 	boolean up, down, left, right, space;
 	int inv;
 
-//	Sword s;
-//	Gun g;
-
 	public Player(int x, int y, int w, int h, Game g) {
 		super(x, y, w, h, g);
 		up = down = left = right = space = false;
 		angle = 0;
 		vx = vy = 0;
 
-//		s = new Sword(x, y, 20, 30, this);
-//		g = new Gun(x + w / 4, y - 5, 10, 10);
+		melee = new Line2D.Double(hitbox.getCenterX(), hitbox.getCenterY(), hitbox.getCenterX(), hitbox.getCenterY());
 		health = 50;
 		inv = 0;
+		sprites = new Image[32][5];
+		try {
+			sprites = Game.generateSprites(sprites, ImageIO.read(getClass().getResource("/game/spriteSheets/Test.png")),
+					16, 20);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void render(Graphics2D g) {
-		// s.render(g);
 		g.setColor(new Color(255, 255, 255));
-		g.fill(hitbox);
-		g.draw(new Line2D.Double(hitbox.getCenterX(), hitbox.getCenterY(), hitbox.getCenterX() + vx * 20,
-				hitbox.getCenterY() + vy * 20));
+		g.drawImage(sprites[d.dir][a.val], (int) hitbox.getX(), (int) hitbox.getY(), (int) hitbox.getX() + W,
+				(int) hitbox.getY() + H, 0, 0, 16, 20, null);
+		g.draw(hitbox);
+		if (a == action.melee)
+			g.draw(melee);
+		getDirection();
 		tick();
 	}
 
 	public void tick() {
+		action();
+
 		if (up && !down)
 			vy = -speed;
 		else if (!up && down)
@@ -59,6 +70,25 @@ class Player extends Character {
 
 		if (!(vy == vx && vy == 0))
 			angle = Math.atan2(vy, vx);
+	}
+
+	public void action() {
+		if (space)
+			if (w == weapon.gun)
+				shoot(50);
+			else if (w == weapon.sword)
+				melee(45);
+
+		if (cooldown > 0) {
+			cooldown--;
+			if (a == action.melee) {
+				double newAngle = angle + Math.toRadians(45) - Math.toRadians(cooldown * 2);
+				melee = new Line2D.Double(hitbox.getCenterX(), hitbox.getCenterY(),
+						hitbox.getCenterX() + 20 * Math.cos(newAngle), hitbox.getCenterY() + 20 * Math.sin(newAngle));
+			}
+		} else {
+			a = action.none;
+		}
 	}
 
 	public void damage(int val) {
